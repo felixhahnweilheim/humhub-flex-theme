9de916487b7735808d05f62b080402ce64dc54f0
Felix Hahn <info@hahn-felix.de>
2023-02-22 21:49:43 +0100

    Update Events.php

--- Events.php
+++ Events.php
@@ -20,5 +20,25 @@ class Events
         if ($theme !== null) {
             $theme->activate();
         }
+        /*Add Module settings*/
+		$module = Yii::$app->getModule('flex-theme');
+		$module->settings->set('commentLink', $module->commentLink);
+	    $module->settings->set('likeLink', $module->likeLink);
+		$module->settings->set('likeIcon', $module->likeIcon);
     }
+    
+	public static function onAdminMenuInit($event)
+    {
+		$event->sender->addItem([
+            'label' =>  Yii::t('FlexThemeModule.base', 'Flex Theme Configuration'),
+            'url' => Url::to(['/flex-theme/config']),
+            'group' => 'manage',
+            'icon' => '<i class="fa fa-eye"></i>',
+            'isActive' => (Yii::$app->controller->module
+                    && Yii::$app->controller->module->id === 'flex-theme'
+                    && (Yii::$app->controller->id === 'page' || Yii::$app->controller->id === 'config')),
+            'sortOrder' => 900,
+            'isVisible' => true,
+        ]);
+	}
 }
