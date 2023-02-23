4e751a6aba58a0d4e3e17ae9f1bb0714a2658cd3
Felix Hahn <info@hahn-felix.de>
2023-02-23 20:49:59 +0100

    Create admin.php

---
+++ messages/de/admin.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+    '<b>Flex Theme</b> Configuration' => '<b>Flex Theme</b>-Konfiguration',
+    'Warning: Flex Theme is not active, settings on this page won\'t have any effect!' => 'Warnung: Flex Theme is nicht aktiv, Einstellungen auf dieser Seite haben keinen Effekt!',
+	'Please go to Administration > Settings > Design and select "Flex Theme".' => 'Bitte gehe zu Administration > Einstellungen > Darstellung und wähle das "Flex Theme" aus.',
+	'Style of Comment Button' => 'Art des "Kommentieren"-Links',
+	'Style of Like Button' => 'Art des "Gefällt mir"-Links',
+	'Like Icon' => 'Icon für "Gefällt mir"',
+);
