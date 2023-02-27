338ee9cd8d3044bd255e74c126e2e6d55d39882e
Felix Hahn <info@hahn-felix.de>
2023-02-27 22:45:28 +0100

    Update head.php

--- themes/FlexTheme/views/layouts/head.php
+++ themes/FlexTheme/views/layouts/head.php
@@ -2,16 +2,13 @@
 //FlexTheme
 use humhub\modules\flexTheme\Module;
 
+$color_vars = array('default', 'primary', 'info', 'success', 'warning', 'danger', 'link');
 $theme = Yii::$app->view->theme;
 ?>
 <style>
 :root {
---default:<?= (Module::getSetting('var_default') ??  $theme->variable('default')). ';'; ?>
---primary:<?= (Module::getSetting('var_primary') ?? $theme->variable('primary')) . ';'; ?>
---info:<?= (Module::getSetting('var_info') ?? $theme->variable('info')) . ';'; ?>
---success:<?= (Module::getSetting('var_success') ?? $theme->variable('success')) . ';'; ?>
---warning:<?= (Module::getSetting('var_warning') ?? $theme->variable('warning')) . ';'; ?>
---danger:<?= (Module::getSetting('var_danger') ?? $theme->variable('danger')) . ';'; ?>
---link:<?= (Module::getSetting('var_link') ?? $theme->variable('link')) . ';'; ?>
+<?php foreach ($color_vars as $color): ?>
+  --<?= $color . ':' . (Module::getSetting($color) ??  $theme->variable($color)) . ';'; ?>
+<?php endforeach; ?>
 }
 </style>
