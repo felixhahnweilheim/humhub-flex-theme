f6df6a6c86f169a9cd5d0507b084dcb64a72cac0
Felix Hahn <info@hahn-felix.de>
2023-02-27 22:30:53 +0100

    Update head.php

--- themes/FlexTheme/views/layouts/head.php
+++ themes/FlexTheme/views/layouts/head.php
@@ -1,16 +1,17 @@
 <?php
 //FlexTheme
-$theme = Yii::$app->view->theme;
+use humhub\modules\flexTheme\Module;
 
+$theme = Yii::$app->view->theme;
 ?>
 <style>
 :root {
---default:<?= $theme->variable('default') . ';'; ?>
---primary:<?= $theme->variable('primary') . ';'; ?>
---info:<?= $theme->variable('info') . ';'; ?>
---success:<?= $theme->variable('success') . ';'; ?>
---warning:<?= $theme->variable('warning') . ';'; ?>
---danger:<?= $theme->variable('danger') . ';'; ?>
---link:<?= $theme->variable('info') . ';'; ?>
+--default:<?= (Module::getSetting('var_default') ??  $theme->variable('default')). ';'; ?>
+--primary:<?= (Module::getSetting('var_primary') ?? $theme->variable('primary')) . ';'; ?>
+--info:<?= (Module::getSetting('var_info') ?? $theme->variable('info')) . ';'; ?>
+--success:<?= (Module::getSetting('var_success') ?? $theme->variable('success')) . ';'; ?>
+--warning:<?= (Module::getSetting('var_warning') ?? $theme->variable('warning')) . ';'; ?>
+--danger:<?= (Module::getSetting('var_danger') ?? $theme->variable('danger')) . ';'; ?>
+--link:<?= (Module::getSetting('var_link') ?? $theme->variable('link')) . ';'; ?>
 }
 </style>
