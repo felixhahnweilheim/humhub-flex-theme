6e3d465082c03e7edf521989dd7807485c6cc432
Felix Hahn <info@hahn-felix.de>
2023-03-05 20:45:27 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -5,8 +5,6 @@ namespace humhub\modules\flexTheme;
 use humhub\modules\ui\view\helpers\ThemeHelper;
 use Yii;
 
-// to be done : fix/move enable function
-
 class Module extends \humhub\components\Module
 {
 
@@ -17,9 +15,9 @@ class Module extends \humhub\components\Module
     public $likeLink = 'text';
     /*@var string defines the like icon (options: heart, thumbs_up, star)*/
     public $likeIcon = 'thumbs_up';
-	/*@var array defines IDs of verified accounts*/
-	public $verifiedAccounts = '';
-	/* color variables*/
+    /*@var array defines IDs of verified accounts*/
+    public $verifiedAccounts = '';
+    /* color variables*/
     public $default;
     public $primary;
     public $info;
@@ -28,47 +26,30 @@ class Module extends \humhub\components\Module
     public $danger;
     public $link;
     
+    // Translatable Module Description
     public function getDescription() {
         return Yii::t('FlexThemeModule.admin', 'Flexible Theme for HumHub');
     }
-	
+    
+    // Module Activation: Activate Flex Theme
     public function enable() {
-		// Activate Flex Theme
+	// Activate Flex Theme
         if (parent::enable()) {
             $theme = ThemeHelper::getThemeByName('FlexTheme');
             if ($theme !== null) {
                 $theme->activate();
-                DynamicConfig::rewrite();
-	        }
-        }
-		
-        /*Add Module settings*/		
-		$this->save('commentLink');
-		$this->save('likeLink');
-		$this->save('likeIcon');
-		$this->save('verifiedAccounts');
-		$this->save('default');
-		$this->save('primary');
-		$this->save('info');
-		$this->save('success');
-		$this->save('warning');
-		$this->save('danger');
-		$this->save('link');
+	    }
+        }	
     }
 	
-    public function save($setting_name) {
-		Yii::$app->getModule('flex-theme')->settings->set($setting_name, $this->$setting_name);
-    }
-	
+    // Module Deactivarion: Deselect Flex Theme (activate community theme)
     public function disable() {
-    
-	    // Deselect theme (select community theme)
-		if (Yii::$app->view->theme->name == 'FlexTheme') {
-		    $theme = ThemeHelper::getThemeByName('HumHub');
-		    if ($theme !== null) {
-		    	$theme->activate();
-			}
-		}
+        if (Yii::$app->view->theme->name == 'FlexTheme') {
+	    $theme = ThemeHelper::getThemeByName('HumHub');
+	    if ($theme !== null) {
+                $theme->activate();
+	    }
+        }
 	
         parent::disable();
     }
