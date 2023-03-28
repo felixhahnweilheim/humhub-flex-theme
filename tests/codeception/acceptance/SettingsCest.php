e304979e7297c6b04529c3815134a06012768bc4
Felix Hahn <info@hahn-felix.de>
2023-03-28 23:41:16 +0200

    Update SettingsCest.php

--- tests/codeception/acceptance/SettingsCest.php
+++ tests/codeception/acceptance/SettingsCest.php
@@ -12,6 +12,7 @@ class SettingsCest
         $I->selectOption('#config-commentlink', 'icon');
         $I->selectOption('#config-likelink', 'icon');
         $I->selectOption('#config-likeicon', 'heart');
+        $I->fillField('config-verifiedaccounts', '1');
         $I->click('Save');
 
         $I->seeSuccess();
