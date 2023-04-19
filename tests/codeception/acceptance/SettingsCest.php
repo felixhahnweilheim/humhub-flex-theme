e2b66da449ac7a7da46e75cea6f9659536b06a5e
Felix Hahn <info@hahn-felix.de>
2023-04-19 23:20:12 +0200

    Update SettingsCest.php

--- tests/codeception/acceptance/SettingsCest.php
+++ tests/codeception/acceptance/SettingsCest.php
@@ -7,8 +7,9 @@ class SettingsCest
     public function testSettings(AcceptanceTester $I)
     {
         $I->amAdmin();
+        $I->enableModule(1, 'calendar');
         $I->amOnRoute(['/flex-theme/config']);
-        // $I->waitForText('Flex Theme');
+        $I->waitForText('Flex Theme');
         $I->selectOption('#config-commentlink', 'icon');
         $I->selectOption('#config-likelink', 'icon');
         $I->selectOption('#config-likeicon', 'heart');
