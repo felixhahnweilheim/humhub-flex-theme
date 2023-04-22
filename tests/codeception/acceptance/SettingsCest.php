9b5b869651a4f72391c215a7c591df5651c8c4c9
Felix Hahn <info@hahn-felix.de>
2023-04-22 07:39:31 +0200

    add default icons etc.
    remove unused code
    fix tests

--- tests/codeception/acceptance/SettingsCest.php
+++ tests/codeception/acceptance/SettingsCest.php
@@ -12,14 +12,6 @@ class SettingsCest
         $I->selectOption('#config-commentlink', 'icon');
         $I->selectOption('#config-likelink', 'icon');
 
-        $I->fillField('#select2-config-likeicon-container', 'heart-o');
-        $I->waitForText('heart-o', 30);
-        $I->pressKey('#select2-config-likeicon-container', \Facebook\WebDriver\WebDriverKeys::ENTER);
-
-        $I->fillField('#select2-config-likeiconfull-container', 'heart-o');
-        $I->waitForText('heart-o', 30);
-        $I->pressKey('#select2-config-likeiconfull-container', \Facebook\WebDriver\WebDriverKeys::ENTER);
-
         $I->click('Save');
 
         $I->seeSuccess();
