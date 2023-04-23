1cf839b6c625febc25790d6db1fd9dc14d9011f2
Felix Hahn <info@hahn-felix.de>
2023-04-23 23:35:19 +0200

    Enh: Background colors (#28)
    * Enh: add background colors
    
    * Enh: background colors (update CSS)
    
    * Update ColorSettings.php
    
    * Update ColorSettings.php
    
    * Update translations

--- views/config/colors.php
+++ views/config/colors.php
@@ -4,20 +4,32 @@ use humhub\modules\flexTheme\models\ColorSettings;
 use humhub\modules\ui\form\widgets\ActiveForm;
 use kartik\widgets\ColorInput;
 use yii\helpers\Html;
-use yii\base\Theme;
-use yii\helpers\Url;
 
-$color_vars = array_merge(ColorSettings::MAIN_COLORS, ColorSettings::TEXT_COLORS);
+$main_colors = ColorSettings::MAIN_COLORS;
+$text_colors = ColorSettings::TEXT_COLORS;
+$background_colors = ColorSettings::BACKGROUND_COLORS;
 ?>
 <div class="panel panel-default">
     <div class="panel-body">
         <?php $form = ActiveForm::begin(['id' => 'configure-form']);?>
 
-	         <div class="color-fields">
-		        <?php foreach ($color_vars as $color): ?>
+            <?= $form->beginCollapsibleFields(Yii::t('FlexThemeModule.admin', 'Main Colors')); ?>
+		        <?php foreach ($main_colors as $color): ?>
 			        <?= $form->field($model, $color)->widget(ColorInput::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select color ...')]]); ?>
 			    <?php endforeach; ?>
-			  </div>
+            <?= $form->endCollapsibleFields(); ?>
+
+			<?= $form->beginCollapsibleFields(Yii::t('FlexThemeModule.admin', 'Text Colors')); ?>
+		        <?php foreach ($text_colors as $color): ?>
+			        <?= $form->field($model, $color)->widget(ColorInput::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select color ...')]]); ?>
+			    <?php endforeach; ?>
+            <?= $form->endCollapsibleFields(); ?>
+
+			<?= $form->beginCollapsibleFields(Yii::t('FlexThemeModule.admin', 'Background Colors')); ?>
+		        <?php foreach ($background_colors as $color): ?>
+			        <?= $form->field($model, $color)->widget(ColorInput::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select color ...')]]); ?>
+			    <?php endforeach; ?>
+            <?= $form->endCollapsibleFields(); ?>
 
         <div class="form-group">
             <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
