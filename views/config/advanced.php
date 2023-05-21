a9d3ef3c7e2632416f30617bcd4b8a203c27c1bc
Felix Hahn <info@hahn-felix.de>
2023-05-21 12:22:59 +0200

    improve admin views

--- views/config/advanced.php
+++ views/config/advanced.php
@@ -5,19 +5,17 @@ use humhub\modules\ui\form\widgets\ActiveForm;
 use yii\helpers\Html;
 
 ?>
-<div class="panel panel-default">
-    <div class="panel-body">
-        <?php $form = ActiveForm::begin(['id' => 'configure-form']);?>
+<div class="panel-body">
+    <?php $form = ActiveForm::begin(['id' => 'configure-form']);?>
 
-            <?= $form->beginCollapsibleFields(Yii::t('FlexThemeModule.admin', 'Import/Export')); ?>
-		        <?= $form->field($model, 'settingsJson')->textarea(['rows' => '12']); ?>
-            <?= $form->endCollapsibleFields(); ?>
+        <?= $form->beginCollapsibleFields(Yii::t('FlexThemeModule.admin', 'Import/Export')); ?>
+            <?= $form->field($model, 'settingsJson')->textarea(['rows' => '12']); ?>
+        <?= $form->endCollapsibleFields(); ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
-
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
     </div>
+
+    <?php ActiveForm::end(); ?>
+
 </div>
--- views/config/index.php
+++ views/config/index.php
@@ -9,29 +9,27 @@ use yii\base\Theme;
 use yii\helpers\Url;
 
 ?>
-<div class="panel panel-default">
-    <div class="panel-body">
-        <?php $form = ActiveForm::begin(['id' => 'configure-form']);?>
+<div class="panel-body">
+    <?php $form = ActiveForm::begin(['id' => 'configure-form']);?>
 
-		    <?= $form->field($model, 'commentLink')->radioList([
-	            'text' => Yii::t('FlexThemeModule.admin', 'Text link'),
-	            'icon' => Yii::t('FlexThemeModule.admin', 'Only icon'),
-	            'both' => Yii::t('FlexThemeModule.admin', 'Text and icon'),
-            ]);?>
-		    <?= $form->field($model, 'likeLink')->radioList([
-	            'text' => Yii::t('FlexThemeModule.admin', 'Text link'),
-	            'icon' => Yii::t('FlexThemeModule.admin', 'Only icon'),
-	            'both' => Yii::t('FlexThemeModule.admin', 'Text and icon'),
-            ]);?>
-            <?= $form->field($model, 'likeIcon')->widget(IconPicker::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select icon ...')]]); ?>
-            <?= $form->field($model, 'likeIconFull')->widget(IconPicker::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select icon ...')]]); ?>
-            <?= $form->field($model, 'likeIconColor')->widget(ColorInput::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select color ...')]]); ?>
+        <?= $form->field($model, 'commentLink')->radioList([
+            'text' => Yii::t('FlexThemeModule.admin', 'Text link'),
+            'icon' => Yii::t('FlexThemeModule.admin', 'Only icon'),
+            'both' => Yii::t('FlexThemeModule.admin', 'Text and icon'),
+        ]);?>
+        <?= $form->field($model, 'likeLink')->radioList([
+            'text' => Yii::t('FlexThemeModule.admin', 'Text link'),
+            'icon' => Yii::t('FlexThemeModule.admin', 'Only icon'),
+            'both' => Yii::t('FlexThemeModule.admin', 'Text and icon'),
+        ]);?>
+        <?= $form->field($model, 'likeIcon')->widget(IconPicker::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select icon ...')]]); ?>
+        <?= $form->field($model, 'likeIconFull')->widget(IconPicker::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select icon ...')]]); ?>
+        <?= $form->field($model, 'likeIconColor')->widget(ColorInput::class, ['options' => ['placeholder' => \Yii::t('FlexThemeModule.admin', 'Select color ...')]]); ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
-
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
     </div>
+
+    <?php ActiveForm::end(); ?>
+
 </div>
