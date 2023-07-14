728efbb52dd1fb28e6e483d59b04224e396a8543
Felix Hahn <info@hahn-felix.de>
2023-07-14 12:37:59 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,8 @@
 <?php
 
-use yii\base\Event;
-use humhub\components\ModuleManager;
-use humhub\modules\admin\widgets\AdminMenu;
-use humhub\modules\flexTheme\Events;
-
 return [
     'id' => 'flex-theme',
     'class' => 'humhub\modules\flexTheme\Module',
     'namespace' => 'humhub\modules\flexTheme',
-    'events' => [
-	    	[
-			'class' => AdminMenu::class,
-			'event' => AdminMenu::EVENT_INIT,
-			'callback' => [Events::class, 'onAdminMenuInit']
-		],
-	]
+    'events' => []
 ];
