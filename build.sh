d1bf584c2a674623b26903fd86d79988aee9bca1
Felix Hahn <info@hahn-felix.de>
2023-10-01 23:24:10 +0200

    add dark-base.css

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cd themes/FlexTheme
 lessc -x ./less/build.less ./css/theme.css
 
 // TO DO css-color-extractor ??
-//lessc -x ./less/dark/build.less ./css/dark-base.css
+lessc -x ./less/dark/build.less ./css/dark-base.css
 
 cd ../../
