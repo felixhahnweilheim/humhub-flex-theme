d12b85f776c1efa3d7fe5996881dca2b92bd09c5
Felix Hahn <info@hahn-felix.de>
2023-12-28 23:59:03 +0100

    Update SettingsCest.php

--- tests/codeception/acceptance/SettingsCest.php
+++ tests/codeception/acceptance/SettingsCest.php
@@ -16,7 +16,6 @@ class SettingsCest
         $I->selectOption('#config-likelink', 'icon');
 
         $I->click('Save');
-
         $I->seeSuccess();
     }
     
@@ -25,29 +24,23 @@ class SettingsCest
         $I->amAdmin();
         $I->amOnRoute(['/flex-theme/config/colors']);
         $I->waitForText('Flex Theme');
-        $I->jsClick('.form-collapsible-fields:nth-child(3)');
+        $I->jsClick('.form-collapsible-fields:nth-child(3) > div');
         $I->see('Background Color Page');
         $I->fillField('ColorSettings[background_color_page]', self::BG_COLOR_PG);
 
         $I->click('Save');
-
         $I->seeSuccess();
-    }
-    
-    public function testEffectOfPreviousSettings(AcceptanceTester $I)
-    {
-        $I->amOnRoute(['/dashboard']);
-        $I->waitForElementVisible('#layout-content');
-
+        
+        // Test effect
         $color = $I->executeJS("getComputedStyle(document.documentElement).getPropertyValue('--background_color_page');");
         $colorDarkened = $I->executeJS("getComputedStyle(document.documentElement).getPropertyValue('--background_color_page__darken__5');");
         
         if ($color !== self::BG_COLOR_PG) {
-            $I->fail();//unknown function
+            throw new Exception("Unexpected color: $color");
         }
         
         if ($colorDarkened !== self::BG_COLOR_PG_D_5) {
-            $I->fail();//unknown function
+            throw new Exception("Unexpected color (darkened): $colorDarkened");
         }
     }
 }
