55d4e1cbd8706410af1b73670af3b7dad85c437a
Felix Hahn <info@hahn-felix.de>
2023-12-28 22:23:28 +0100

    Update SettingsCest.php

--- tests/codeception/acceptance/SettingsCest.php
+++ tests/codeception/acceptance/SettingsCest.php
@@ -4,8 +4,8 @@ use flexTheme\AcceptanceTester;
 
 class SettingsCest
 {
-    $bg_color_pg = '#cfe2f3';
-    $bg_color_pg_d_5 = '#c3d5e5';
+    const BG_COLOR_PG = '#cfe2f3';
+    const BG_COLOR_PG_D_5 = '#c3d5e5';
     
     public function testSettings(AcceptanceTester $I)
     {
@@ -25,7 +25,7 @@ class SettingsCest
         $I->amAdmin();
         $I->amOnRoute(['/flex-theme/config/colors']);
         $I->waitForText('Flex Theme');
-        $I->fillField('Background Color Page', $this->bg-color-pg);
+        $I->fillField('Background Color Page', self::BG_COLOR_PG);
 
         $I->click('Save');
 
@@ -40,11 +40,11 @@ class SettingsCest
         $color = $I->executeJS("getComputedStyle(document.documentElement).getPropertyValue('--background_color_page');");
         $colorDarkened = $I->executeJS("getComputedStyle(document.documentElement).getPropertyValue('--background_color_page__darken__5');");
         
-        if ($color !== $this->bg_color_pg) {
+        if ($color !== self::BG_COLOR_PG) {
             return false;
         }
         
-        if ($colorDarkened !== $this->bg_color_pg_d_5) {
+        if ($colorDarkened !== self::BG_COLOR_PG_D_5) {
             return false;
         }
     }
