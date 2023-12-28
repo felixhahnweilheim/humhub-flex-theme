584cc619116c415a881492420cce04afabb6f50c
Felix Hahn <info@hahn-felix.de>
2023-12-28 21:25:44 +0100

    add tests

--- tests/codeception/acceptance/SettingsCest.php
+++ tests/codeception/acceptance/SettingsCest.php
@@ -4,6 +4,8 @@ use flexTheme\AcceptanceTester;
 
 class SettingsCest
 {
+    $bg_color_pg = '#cfe2f3';
+    
     public function testSettings(AcceptanceTester $I)
     {
         $I->amAdmin();
@@ -15,5 +17,29 @@ class SettingsCest
         $I->click('Save');
 
         $I->seeSuccess();
+    }
+    
+    public function testSettingsColor(AcceptanceTester $I)
+    {
+        $I->amAdmin();
+        $I->amOnRoute(['/flex-theme/config/colors']);
+        $I->waitForText('Flex Theme');
+        $I->fillField('Background Color Page', $this->bg-color-pg);
+
+        $I->click('Save');
+
+        $I->seeSuccess();
+    }
+    
+    public function testEffectOfPreviousSettings(AcceptanceTester $I)
+    {
+        $I->amOnRoute(['/dashboard']);
+        $I->waitForElementVisible('#wallStream');
+
+        $color = $I->executeJS("getComputedStyle(document.documentElement).getPropertyValue('--background_color_page');");
+        
+        if ($color !== $this->bg_color_pg) {
+            return false;
+        }
     }
 }
