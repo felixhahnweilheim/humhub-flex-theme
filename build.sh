8ea71a017625902d34a167d67badb4764d05c4e2
Felix Hahn <info@hahn-felix.de>
2024-02-08 22:16:42 +0100

    dark mode include whole theme

--- build.sh
+++ build.sh
@@ -5,7 +5,17 @@ cd themes/FlexTheme
 lessc less/build.less css/theme_base.css --clean-css="--s1 --advanced"
 cd ../../
 
-# 2) Update message files
+# 2) Recompile dark CSS
+cd themes/FlexTheme
+lessc less/dark_build.less css/dark_theme_base.css
+css-color-extractor css/dark_theme.css css/dark_theme.css --format=css
+# Re-add CSS variables and compress CSS
+cp css/dark_theme.css css/dark_temporary.less
+lessc less/dark_build2.less css/dark_theme.css --clean-css="--s1 --advanced"
+rm css/dark_temporary.less
+cd ../../
+
+# 3) Update message files
 cd ../../
 php yii message/extract-module flex-theme
 cd modules/flex-theme
