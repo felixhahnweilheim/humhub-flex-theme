b19e358fef998b15c7f865d0903dfdf801cbe4b4
Felix Hahn <info@hahn-felix.de>
2024-02-08 23:26:49 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ cd themes/FlexTheme
 lessc less/build.less css/theme_base.css --clean-css="--s1 --advanced"
 cd ../../
 
+# 2) Dark Mode
+cd themes/FlexTheme
+lessc less/dark_build.less css/dark_theme_base.css --clean-css="--s1 --advanced"
+cd ../../
+
 # 2) Update message files
 cd ../../
 php yii message/extract-module flex-theme
